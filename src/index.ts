9472e3fffef3e58f9e78b2d63f7359e2855d2266
joemrk <o9ino4ka@gmail.com>
2020-10-11 01:31:01 +0300

    init app

---
+++ src/index.ts
@@ -0,0 +1,46 @@
+import 'reflect-metadata'
+import { MikroORM } from '@mikro-orm/core'
+import { __prod__ } from './constants'
+// import { Post } from './entities/Post'
+import microOrmConfig from './mikro-orm.config'
+import express from 'express'
+import {ApolloServer} from 'apollo-server-express'
+import {buildSchema} from 'type-graphql'
+import { HelloResolver } from './resolvers/hello'
+import { PostResolver } from './resolvers/post';
+
+
+// const PORT = process.env.PORT || 5000
+
+const main = async () => {
+  const orm = await MikroORM.init(microOrmConfig)
+  await orm.getMigrator().up()
+
+  const app = express()
+  const apollosServer = new  ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver, PostResolver],
+      validate: false
+    }),
+    context: ()=> ({em: orm.em})
+  })
+  apollosServer.applyMiddleware({app})
+
+  // app.get('/', (_, res)=>{
+  //   res.status(200).json({data: 'hello'})
+  // })
+
+
+  app.listen(4000, () => {
+    console.log(`Server has been started on localhost:4000`);
+  })
+  // const posta = orm.em.create(Post, {title: 'first posta 22'})
+  // await orm.em.persistAndFlush(posta)
+
+  // const posts = await orm.em.find(Post, {})
+  // console.log(posts);
+
+}
+
+main()
+
